ce63479b0317fa1d997e21a0bb31d7a92625eefe
Matthew Kirby <mkirby528@Matthews-MacBook-Air.local>
2023-03-14 00:31:41 -0400

    add flip action

--- src/App/AlbumCard/index.js
+++ src/App/AlbumCard/index.js
@@ -2,16 +2,19 @@ import React from "react";
 import "./index.css"
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Tooltip from 'react-bootstrap/Tooltip';
+import ReactCardFlip from 'react-card-flip';
 
 class AlbumCard extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            album: this.props.album
+            album: this.props.album,
+            isFlipped: false
         }
     }
-    flipCard = () => {
-        alert("Flipping the card yo")
+    flipCard = (e) => {
+        e.preventDefault();
+        this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
     }
     renderTooltip = (props) => (
         <Tooltip id="button-tooltip" {...props}>
@@ -22,37 +25,43 @@ class AlbumCard extends React.Component {
 
     render() {
         return (
-            <div className="album-card">
-                <div className="card-top">
-                    <div className="card-top-column">
-                        <img className="album-cover-image" alt={`${this.state.album.Title} album cover`} src={this.state.album.Images.md} />
-                    </div>
-                    <div className="card-top-column">
-                        <div className="ratings-container">
-                            <span className="rating-text">{`${this.state.album.Rating}`}</span>
+            <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal" containerClassName="album-card">
+                <div className="card-front">
+                    <div className="card-top">
+                        <div className="card-top-column">
+                            <img className="album-cover-image" alt={`${this.state.album.Title} album cover`} src={this.state.album.Images.md} />
                         </div>
-                        <div className="play-now-container">
-                            <a className="spotify-logo-link" href={this.state.album.SpotifyURI} target="_blank" rel="noreferrer">
-                                <img className="spotify-logo" src="/spotify_logo.svg" alt="spotify logo" />
-                            </a>
+                        <div className="card-top-column">
+                            <div className="ratings-container">
+                                <span className="rating-text">{`${this.state.album.Rating}`}</span>
+                            </div>
+                            <div className="play-now-container">
+                                <a className="spotify-logo-link" href={this.state.album.SpotifyURI} target="_blank" rel="noreferrer">
+                                    <img className="spotify-logo" src="/spotify_logo.svg" alt="spotify logo" />
+                                </a>
+                            </div>
                         </div>
                     </div>
+                    <div className="card-bottom">
+                        <OverlayTrigger
+                            placement="top"
+                            delay={{ show: 250, hide: 400 }}
+                            overlay={this.renderTooltip}
+                        >
+                            <h1 className="album-title-text">{`${this.state.album.Title}`} </h1>
+                        </OverlayTrigger>
+                        <h2 className="album-title-text">{`${this.state.album.Artist}`} </h2>
+                    </div>
+                    <i onClick={this.flipCard} className="fa-solid fa-rotate flip-icon"></i>
                 </div>
-                <div className="card-bottom">
-                    <OverlayTrigger
-                        placement="top"
-                        delay={{ show: 250, hide: 400 }}
-                        overlay={this.renderTooltip}
-                    >
-                        <h1 className="album-title-text">{`${this.state.album.Title}`} </h1>
-                    </OverlayTrigger>
-                    <h2 className="album-title-text">{`${this.state.album.Artist}`} </h2>
+                <div className="card-back">
+                    <i onClick={this.flipCard} className="fa-solid fa-rotate flip-icon"></i>
+
                 </div>
-                <i onClick={this.flipCard} className="fa-solid fa-rotate flip-icon"></i>
-            </div>)
+            </ReactCardFlip>
+        )
 
     }
 }
-
 
 export default AlbumCard
