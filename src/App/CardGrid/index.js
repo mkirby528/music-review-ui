9830ad809ca90ce81e93960e6ad0c247205c73fb
Matthew Kirby <mkirby528@Matthews-MacBook-Air.local>
2023-03-24 12:26:44 -0400

    update year filter to be inclusive of min and max year

--- src/App/CardGrid/index.js
+++ src/App/CardGrid/index.js
@@ -38,20 +38,22 @@ class CardGrid extends React.Component {
         const filterByYear = (album) => {
             const releaseDate = album.ReleaseDate
             const releaseYear = (releaseDate.substr(releaseDate.length - 4))
-            return releaseYear > minYear && releaseYear < maxYear
+            return releaseYear >= minYear && releaseYear <= maxYear
         }
         const KEYS_TO_FILTERS = ['Title', 'Artist']
         let filteredAlbums = this.state.albums.filter(createFilter(this.props.filterValues.searchTerm, KEYS_TO_FILTERS))
         filteredAlbums = filteredAlbums.filter(filterByYear)
+        console.log(filteredAlbums)
 
 
         return (
             <Row xs={1} md={3} className="card-grid">
                 {filteredAlbums.map(album => (
-                    <Col key={album.id} className="card-container">
+                    < Col key={album.id} className="card-container" >
                         <AlbumCard album={album} />
                     </Col>)
-                )}
+                )
+                }
             </Row>
         )
     }
