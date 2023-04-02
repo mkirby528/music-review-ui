5f5364e69ac6a3c5a87341c8fc57fd57971b7bc4
Matthew Kirby <mkirby528@Matthews-MacBook-Air.local>
2023-04-01 21:26:02 -0400

    better styling of release date range

---
+++ src/App/Header/ReleaseDateSlider/index.js
@@ -0,0 +1,43 @@
+import "./index.css"
+import React from "react";
+import { Slider } from "@mui/joy";
+import { updateDateRange } from "../../Store/actions";
+import { connect } from "react-redux";
+
+class ReleaseDateSlider extends React.Component {
+    constructor(props) {
+        super(props)
+        this.releaseDateRangeUpdated = this.releaseDateRangeUpdated.bind(this)
+    }
+    releaseDateRangeUpdated(event, dateRange) {
+        this.props.updateDateRange(dateRange[0], dateRange[1])
+    }
+    render() {
+        return (
+            <div className="release-year-container">
+                <h6 className="releaseyear-label">
+                    Release Year
+                </h6>
+                <Slider
+                    id="release-year-slider"
+                    getAriaLabel={() => 'Temperature range'}
+                    min={1965}
+                    max={2023}
+                    defaultValue={[1965, 2023]}
+                    valueLabelDisplay="on"
+                    onChange={this.releaseDateRangeUpdated}
+                />
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    const { albumsStore } = state
+    return {
+        albums: albumsStore.albums,
+        filterValues: albumsStore.filterValues,
+        isLoading: albumsStore.isLoading
+    }
+}
+export default connect(mapStateToProps, { updateDateRange })(ReleaseDateSlider);
--- src/App/Header/index.js
+++ src/App/Header/index.js
@@ -1,38 +1,22 @@
 import "./index.css"
 import React from "react";
 import SearchInput from "react-search-input"
-import Slider from '@mui/joy/Slider';
+import ReleaseDateSlider from "./ReleaseDateSlider";
 import SortSelction from "./SortSelction";
 import { connect } from "react-redux";
-import { updateSearchTerm, updateDateRange, updateSortField } from "../Store/actions";
-import { Row, Col } from "react-bootstrap";
+import { updateSearchTerm, updateDateRange } from "../Store/actions";
+import { Row } from "react-bootstrap";
 class Header extends React.Component {
-    constructor(props) {
-        super(props)
-        this.releaseDateRangeUpdated = this.releaseDateRangeUpdated.bind(this)
-    }
-    releaseDateRangeUpdated(event, dateRange) {
-        this.props.updateDateRange(dateRange[0], dateRange[1])
-    }
+
+
     render() {
         return (
             <Row className="header">
                 <SearchInput className="search-input" fuzzy onChange={this.props.updateSearchTerm} />
-                <Row>
-                    <Col>
-                        <Slider
-                            id="release-year-slider"
-                            getAriaLabel={() => 'Temperature range'}
-                            min={1965}
-                            max={2023}
-                            defaultValue={[1965, 2023]}
-                            valueLabelDisplay="on"
-                            onChange={this.releaseDateRangeUpdated}
-                        />
-                    </Col>
-                    <Col>
-                        <SortSelction></SortSelction>
-                    </Col>
+                <Row className="filter-options-container" xs={2}>
+                    <ReleaseDateSlider></ReleaseDateSlider>
+                    <SortSelction></SortSelction>
+
                 </Row>
             </Row>
         )
@@ -46,4 +30,4 @@ function mapStateToProps(state) {
         isLoading: albumsStore.isLoading
     }
 }
-export default connect(mapStateToProps, { updateSearchTerm, updateDateRange, updateSortField })(Header);
+export default connect(mapStateToProps, { updateSearchTerm, updateDateRange })(Header);
