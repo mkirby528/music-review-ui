d47768970c5d543f6807865bb8a6bc6d36c85758
Matthew Kirby <mkirby2899@gmail.com>
2023-04-03 22:48:18 -0400

    historgram for reviews

--- src/App/Pages/Stats/index.js
+++ src/App/Pages/Stats/index.js
@@ -7,8 +7,18 @@ import { Container } from "react-bootstrap";
 import Header from "../../Components/Header"
 import { connect } from "react-redux";
 import { addAlbums, updateSearchTerm, updateDateRange } from "../../Store/actions";
+import { Chart, registerables } from 'chart.js';
+import { Bar } from "react-chartjs-2";
+Chart.register(...registerables);
 
 class StatsPage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            ratingCounts: {}
+
+        }
+    }
     async fetchAllAlbums() {
         const baseUrl = "https://zjixv0m4di.execute-api.us-east-1.amazonaws.com/non-prod"
         const albumsPath = "/albums"
@@ -23,16 +33,46 @@ class StatsPage extends React.Component {
     async componentDidMount() {
         if (!this.props.albums || this.props.albums.length === 0) {
             const albumsResponse = await this.fetchAllAlbums()
-            this.props.addAlbums(albumsResponse)
+            await this.props.addAlbums(albumsResponse)
+            this.getRatingCouts()
         }
     }
 
-    render() {
+    getRatingCouts() {
+        const ratingCounts = {};
+        this.props.albums.forEach(function (v) {
+            ratingCounts[v.Rating] = (ratingCounts[v.Rating] || 0) + 1;
+        })
+        this.setState({ ratingCounts })
+    }
 
+    render() {
+        const options = {
+            responsive: false,
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Ratings',
+                },
+            },
+        };
+        const labels = Object.keys(this.state.ratingCounts)
+        const data = {
+            labels,
+            datasets: [
+                {
+                    label: 'Count',
+                    data: Object.values(this.state.ratingCounts),
+                    backgroundColor: 'rgba(0, 255, 132, 0.8)',
+                },
+
+            ],
+        };
         return (
             <Container fluid className="app" >
                 <Header />
                 <h1 className="total-review-count-label">Total Reviews: {this.props.albums.length}</h1>
+                <Bar options={options} data={data} />;
 
             </Container >)
     }
