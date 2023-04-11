13a92a79fe2fba0e2e1abb2907274e707ea07992
Matthew Kirby <mkirby2899@gmail.com>
2023-04-10 20:59:18 -0400

    add links in header

--- src/App/Components/Header/index.js
+++ src/App/Components/Header/index.js
@@ -5,19 +5,29 @@ import ReleaseDateSlider from "./ReleaseDateSlider";
 import SortSelction from "./SortSelction";
 import { connect } from "react-redux";
 import { updateSearchTerm, updateDateRange } from "../../Store/actions";
-import { Row } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 class Header extends React.Component {
 
 
     render() {
         return (
             <Row className="header">
-                <SearchInput className="search-input" fuzzy onChange={this.props.updateSearchTerm} />
-                <Row className="filter-options-container" xs={2}>
-                    <ReleaseDateSlider></ReleaseDateSlider>
-                    <SortSelction></SortSelction>
-
-                </Row>
+                <Col xs={11}>
+                    <SearchInput className="search-input" fuzzy onChange={this.props.updateSearchTerm} />
+                    <Row className="filter-options-container" xs={2}>
+                        <ReleaseDateSlider></ReleaseDateSlider>
+                        <SortSelction></SortSelction>
+                    </Row>
+                </Col>
+                <Col className="add-review-column">
+                    <Link to="/add">
+                        <i className="icon-link fa-solid fa-circle-plus"></i>
+                    </Link>
+                    <Link to="/stats">
+                        <i className="icon-link fa-solid fa-chart-line"> </i>
+                    </Link>
+                </Col>
             </Row>
         )
     }
